0573cc8bea6dec9b9cf599ea97c2bd1793f42fce
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-25 18:03:13 +0200

    create axios request

--- js/script.js
+++ js/script.js
@@ -3,7 +3,22 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-
+      email:null
     }
-  }
+  },
+
+  methods:{
+    getApi(){
+      axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then( (result) =>{
+      this.email = result.data.response
+      })
+    },
+    
+  },
+  
+      mounted(){
+        this.getApi();
+      }
 }).mount('#app')
