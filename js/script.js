5e80db599c171704e15c41873d7b6203a08d4346
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-26 09:49:21 +0200

    correction

--- js/script.js
+++ js/script.js
@@ -3,22 +3,28 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      email:null
+      title: 'Email list',
+      apiUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
+      emails:[],
+      limit:10
     }
   },
 
   methods:{
     getApi(){
       axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .get(this.apiUrl)
       .then( (result) =>{
-      this.email = result.data.response
+        // console.log(result.data.response)
+        console.log(this.emails)
+        this.emails.push(result.data.response);
       })
     },
     
   },
   
       mounted(){
+        for(let i = 0 ; i < this.limit; i++)
         this.getApi();
       }
 }).mount('#app')
